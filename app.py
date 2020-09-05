a9a56bdf300cc921e827b941136c50f4b2fc45a6
Chinmay Kabi <chinmay.kabi@live.com>
2020-09-05 19:45:44 +0530

    Minor changes

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ from textgen import TextGenerator
 
 app = Flask(__name__)
 
-model_size = 'gpt2'
-gen = TextGenerator(model_size)
-
 @app.route('/getx/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
@@ -26,8 +23,8 @@ def respond():
         response["ERROR"] = "name can't be numeric."
     # Now the user entered a valid name
     else:
-        excuse= excuseggen.makeExcuse()
-        response["MESSAGE"] = "{excuse}"
+        excuseStr= excuseggen.makeExcuse()
+        response["MESSAGE"] = excuseStr
 
     # Return the response in json format
     return jsonify(response)
@@ -52,7 +49,8 @@ def post_something():
 def excuse():
     def show_word(w):
         print('>', w)
-
+    model_size = 'gpt2'
+    gen = TextGenerator(model_size)
     s = ExcuseSituation(gen, assignment="prepare a nice dinner for you for Valentine's day", tasks=[
         'plan the menu',
         'go to the grocery store to buy the ingredients',
@@ -71,7 +69,7 @@ def index():
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
+    app.run(host='0.0.0.0', port=80)
 
 
 
